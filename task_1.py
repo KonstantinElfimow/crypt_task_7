1c5fb41fcf7950655ad0394bcea1c8d79dd9ead1
Konstantin <k.elfimow@gmail.com>
2022-12-22 12:50:20 +0300

    Initial commit

--- task_1.py
+++ task_1.py
@@ -2,9 +2,6 @@ import hashlib
 
 """ (1): Определить алгоритм H, использовавшийся для формирования хеш-кода h для строки m. """
 
-hash_algorythm: set = {'sha3_512', 'sha384', 'md5', 'sha256', 'sha3_256', 'sha3_224', 'sha512',
-                       'blake2s', 'sha3_384', 'sha224', 'blake2b', 'sha1'}
-
 
 def define_hash_algorythm(*, m: bytes, h: hex) -> str:
     for a in hashlib.algorithms_available:
